7081a65e78f2f4efcd112535efa7e52715d845f6
Frederik Batuna <gobi301@gmail.com>
2015-12-09 11:43:22 +0100

    dropped dispatch function

--- reactive.js
+++ reactive.js
@@ -1,40 +1,39 @@
 (function(window){
 
-window.Reactive = Reactive;
-window.r = function(){ var ret = new Reactive(); Reactive.prototype.r.apply(ret, arguments); return ret};
+window.Reactive = window.Reactive || { _shortcuts: {}};
+window.Reactive.Reactive = Reactive;
+window.Reactive.shortcuts = function Reactive_shortcuts(){ var shortcuts = window.Reactive._shortcuts, i;
+  for (i in shortcuts) window[i] = shortcuts[i];
+};
+window.Reactive._shortcuts.r = function(){ var ret = new Reactive(); return ret};
 
 function Reactive(){
   this.value = undefined;
   this.watchers = [];
   this.watchees = null;
-  Reactive.prototype.r.apply(this, arguments);
 };
 
-Reactive.prototype.r = function react(){ switch (arguments.length) {
-  case 0: return this.value;
-  case 1: return this.s(arguments[0]);
-  default: return Reactive.prototype.m.apply(this,arguments);
-}};
+Reactive.prototype.g = function Reactive_g(){ return this.value };
 
-Reactive.prototype.w = function watch(callback) { this.watchers.push(callback); return this; };
+Reactive.prototype.w = function Reactive_watch(callback) { this.watchers.push(callback); return this; };
 
-Reactive.prototype.u = function unwatch(callback) { var ws = this.watchers; ws.splice(ws.indexOf(callback),1); return this; }
+Reactive.prototype.u = function Reactive_unwatch(callback) { var ws = this.watchers; ws.splice(ws.indexOf(callback),1); return this; }
 
-Reactive.prototype.s = function set(value, isDependency) { if (!isDependency) this.c();
+Reactive.prototype.s = function Reactive_set(value, isDependency) { if (!isDependency) this.c();
   var old = this.value, watcher; this.value = value;
   for (var i in this.watchers) (watcher = this.watchers[i]), watcher(value, old);
   return this;
 };
 
-Reactive.prototype.m = function method(fun) { this.c();
+Reactive.prototype.m = function Reactive_method(fun) { this.c();
   var dependencies = [], args = Array.prototype.slice.call(arguments,1), watchees = [], self = this;
   for (var i in args){ var arg = args[i];
     if (arg instanceof Reactive) arg.w((function(i){
-        var callback = function dependency(delta, old){
+        var callback = function Reactive_dependency(delta, old){
           dependencies[i] = delta;
           self.s(fun.apply(null, dependencies), true);
         };
-        dependencies[i] = arg.r();
+        dependencies[i] = arg.g();
         watchees.push({reactive: arg, callback:callback});
         return callback;
     })(i));
@@ -45,7 +44,7 @@ Reactive.prototype.m = function method(fun) { this.c();
   return this;
 };
 
-Reactive.prototype.c = function clear_dependencies () { var watchees = this.watchees, watchee;
+Reactive.prototype.c = function Reactive_clear_dependencies () { var watchees = this.watchees, watchee;
   for (i in watchees) (watchee = watchees[i]), watchee.reactive.u(watchee.callback); this.watchees = null;
   return this;
 };
