36372cbc0eb1f19741490b84a27efffa3b12b9ac
Frederik Batuna <gobi301@gmail.com>
2015-12-11 21:51:20 +0100

    add: components

--- template.js
+++ template.js
@@ -4,8 +4,9 @@ INDENT_SYMBOL = "\t";
 
 function node(element){
   if (element.nodeName=="#text") return textNode(element);
+  if (element.nodeName=="G-COMPONENT") return element.textContent;
   var tag = "\""+element.tagName.toLowerCase()+"\"", attributes = null, children = null;
-  if (element.attributes.length)  attributes = "{"+_.map(element.attributes, nodeAttribute).join(", ")+"}";
+  if (element.attributes.length)  attributes = "{"+_.compact(_.map(element.attributes, nodeAttribute)).join(", ")+"}";
   if (element.childNodes.length) {
     var effective_children = _.chain(element.childNodes).map(node).compact().value();
     if (effective_children.length == 1) children = effective_children[0];
@@ -25,7 +26,7 @@ function textNode(element){
 
 function checkString(string) { return string[0]=="=" ? string.slice(1): "\""+JSON.stringify(string).slice(1,-1)+"\"" };
 
-function nodeAttribute(att){ return "\""+att.name+"\": " + checkListenerString(att.value) };
+function nodeAttribute(att){ if (att.name.match(/^g-/)) return; return "\""+att.name+"\": " + checkListenerString(att.value) };
 
 function checkListenerString(string){ switch (string[0]) {
   case "#": return "function(event){"+string.slice(1)+";event.preventDefault()}";
@@ -33,13 +34,13 @@ function checkListenerString(string){ switch (string[0]) {
 }};
 
 function repeatElement(element, body){
-  var list = element.getAttribute("g-in"); element.removeAttribute("g-in");
-  var vars = element.getAttribute("g-for"); element.removeAttribute("g-for");
+  var list = element.getAttribute("g-in")
+  var vars = element.getAttribute("g-for");
   return "r().m(_.map, "+list+", function("+vars+"){return "+body+"})";
 }
 
 function letElement(element, body){
-  var vars = element.getAttribute("g-let"); element.removeAttribute("g-let");
+  var vars = element.getAttribute("g-let");
   return "(function(){var "+vars+"; return "+body+"})()"
 }
 
